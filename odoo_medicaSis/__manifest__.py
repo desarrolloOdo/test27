2d780ff7a868adf0b03c354eda0c5c39602bacaa
ivan <odootest725@gmail.com>
2023-01-16 19:46:39 +0000

    [ADD] modulo

---
+++ odoo_medicaSis/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo MedicaSis',
+    
+    'summary':"""medica app to manage date""",
+    
+    'description': """
+        Medica Module to manage :
+        -Medicos
+        -Pacientes
+        -Citas
+    """,
+    
+    'author': 'Ivan',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends':['base'],
+    
+    'data':[
+    
+    ],
+    
+    
+    
+     'license': 'LGPL-3',
+    
+}
